933d023bce245cfa3928c9d25c5a5003bf4e8a64
jesse <jessefortuin850@gmail.com>
2023-07-26 13:35:28 +0200

    Feat: Added http Get

--- apm/src/app/products/product-list.component.ts
+++ apm/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from 'rxjs'
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -8,12 +9,14 @@ templateUrl: './product-list.component.html',
 styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   //TS can infer type if default is given
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin = 2;
   showImage: boolean = false;
+  errorMessage: string = '';
+  sub!: Subscription;
 
   private _listFilter: string = '';
   get listFilter(): string {
@@ -39,9 +42,19 @@ export class ProductListComponent implements OnInit {
   toggleImage(): void {
     this.showImage = !this.showImage;
   };
+
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts =this.products;
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
   onRatingClicked(message : string): void {
